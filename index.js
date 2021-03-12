6cfdcf420c362227fb29e1343c27b53a80311aa5
sparshithNR <snrai@linkedin.com>
2021-03-12 13:47:41 -0800

    Changed public API name Updated README Fix help texts

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 const run = require('./lib/cli');
 const setupProject = require('./lib/project/setup-project');
-const { isAllInSupportWindow } = require('./lib/project/mulitple-projects');
+const { processPolicies } = require('./lib/project/mulitple-projects');
 
 module.exports = {
   run,
   setupProject,
-  isAllInSupportWindow,
+  processPolicies,
 };

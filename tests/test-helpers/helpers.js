e66dff39f25199c3f6b12abd59561f7761c920a3
sparshithNR <snrai@linkedin.com>
2021-03-15 16:31:56 -0700

    Fixing the start registry
    npmrc file do not get packed with library. We will create that in node_module to help with our test

--- tests/test-helpers/helpers.js
+++ tests/test-helpers/helpers.js
@@ -1,12 +1,22 @@
 'use strict';
 
-const { existsSync, readdirSync } = require('fs');
+const { existsSync, readdirSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
 function getTestProjectPath(projectName) {
   const root = `${__dirname}/../fixtures`;
   const projectPath = join(root, projectName);
   if (existsSync(projectPath)) {
+    // when project tar ball skips packaging of npmrc file. So we will have to create the file if it isn't present
+    let npmRc = join(projectPath, '.npmrc');
+    if (!existsSync(npmRc)) {
+      writeFileSync(
+        npmRc,
+        `
+      registry=http://0.0.0.0:3000
+      @stefanpenner:registry=http://0.0.0.0:3001`,
+      );
+    }
     return projectPath;
   }
   throw new Error(`Unable to find ${projectName} at ${root}`);

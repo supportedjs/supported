892c2a02385f4efcea5d3c98afc49ee8167efbe5
Stefan Penner <stefan.penner@gmail.com>
2021-05-04 08:22:49 -0600

    Unit test processDate

--- tests/util-test.js
+++ tests/util-test.js
@@ -1,7 +1,10 @@
 'use strict';
 
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai;
 const { sortLibraries, checkNodeCompatibility } = require('../lib/util');
+
+chai.use(require('chai-datetime'));
 
 describe('util test', function () {
   describe('sort packages', function () {
@@ -102,6 +105,7 @@ describe('util test', function () {
       expect(input.sort(sortLibraries)).to.be.eql(result);
     });
   });
+
   describe('checkNodeCompatibility', function () {
     it(`throws error when node version is equal to 8.*`, function () {
       let regex = new RegExp(
@@ -111,6 +115,7 @@ describe('util test', function () {
         checkNodeCompatibility('8.10.1');
       }).throws(regex);
     });
+
     it(`throws error when node version is below 8.*`, function () {
       let regex = new RegExp(
         `Node v7.10.1 found, which does not satisfy the required version range: v10.* || 12.* || 14.* || >= 15. Please updated the node version.`,
@@ -119,8 +124,33 @@ describe('util test', function () {
         checkNodeCompatibility('7.10.1');
       }).throws(regex);
     });
+
     it(`do nothing when node is above 8.*`, function () {
       checkNodeCompatibility('10.1.1');
+    });
+  });
+
+  describe('processDate', function () {
+    const { processDate } = require('../lib/util');
+
+    it('handles reasonable potential date inputs', function () {
+      expect(processDate()).to.eql(undefined);
+      expect(processDate(1)).to.eql(undefined);
+      expect(processDate(new Date())).to.eql(undefined);
+      expect(processDate('1 day')).to.be.instanceof(Date);
+      expect(processDate('-1 day')).to.be.instanceof(Date);
+      expect(processDate('Sept 16, 1986')).to.be.instanceof(Date);
+      expect(() => processDate('EWUT')).to.throw(/could not parse date/);
+
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+
+      const yesterday = new Date();
+      yesterday.setDate(yesterday.getDate() - 1);
+
+      expect(processDate('1 day')).to.equalDate(tomorrow);
+      expect(processDate('-1 day')).to.equalDate(yesterday);
+      expect(processDate('Sept 16, 1986')).to.equalDate(new Date('Sept 16, 1986'));
     });
   });
 });

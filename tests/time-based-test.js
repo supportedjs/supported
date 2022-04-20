73f8d4603a760c2921e49968e810787557692057
asakusuma <asa.kusuma@gmail.com>
2022-04-20 15:38:49 -0700

    Fix bug when using prereleases
    Using a prerelease was previously failing when using ignorePrereleases

--- tests/time-based-test.js
+++ tests/time-based-test.js
@@ -82,6 +82,31 @@ describe('time based policy: 1 year for major, 6 months for minor, 3 months of p
         true,
       ),
     ).to.eql({ isSupported: true });
+
+    expect(
+      supported(
+        {
+          version: '1.0.0',
+          time: {
+            '1.0.0': 'never',
+          },
+          'dist-tags': {
+            latest: '2.0.0-beta.16',
+          },
+          versions: {
+            '2.0.0-beta.16': {},
+            '1.0.0': {},
+            '0.5.0': {},
+          },
+        },
+        'example@2.0.0-beta.16',
+        [],
+        null,
+        null,
+        null,
+        true,
+      ),
+    ).to.eql({ isSupported: true });
   });
 
   it('accepts pre-releases if all versions are pre-releases, even if configured to ignore pre-releases', function () {
